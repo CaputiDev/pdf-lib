dd1dc69832f7e9537fd954f8321e368a93fc6d88
Caputi <lcaputi2@gmail.com>
2026-06-19 11:28:18 -0300

    docs(swagger): implementation of project endpoints documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('PDF Library')
+    .setDescription('RESTful API to manage, catalog and read PDF files locally')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('pdf-lib', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
