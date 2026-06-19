eb1de2d3e4775c22cf49754fcfbc24458e8fbccd
Caputi <lcaputi2@gmail.com>
2026-06-19 11:06:18 -0300

    build(prisma): add new modules to prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './config/prisma/prisma.module';
 import { DocumentsModule } from './modules/documents/documents.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, DocumentsModule],
+  imports: [PrismaModule, DocumentsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
 
 
